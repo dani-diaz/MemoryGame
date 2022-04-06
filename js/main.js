2e56608a690629ca2788b18a4ef7a0b9c35b8731
dani-diaz <daniela.diaz0808@gmail.com>
2022-04-06 11:34:12 -0700

    new imgs 20 cards play again

---
+++ js/main.js
@@ -0,0 +1,82 @@
+/*----- constants -----*/
+// Each of the "card" objects will be used twice,
+// then shuffled and used for the board's cards
+var SOURCE_CARDS = [
+    {img: 'https://i.imgur.com/kozGuFZ.jpg', matched: false},
+    {img: 'https://i.imgur.com/Lh4jRp2.jpg', matched: false},
+    {img: 'https://i.imgur.com/iKGTz7W.jpg', matched: false},
+    {img: 'https://i.imgur.com/RXfs2Jx.jpg', matched: false},
+    {img: 'https://i.imgur.com/KmQ1yzy.jpg', matched: false},
+    {img: 'https://i.imgur.com/032Xw27.jpg', matched: false},
+    {img: 'https://i.imgur.com/xGc60iY.jpg', matched: false},
+    {img: 'https://i.imgur.com/k5gfso9.jpg', matched: false},
+    {img: 'https://i.imgur.com/1IsAGDl.jpg', matched: false},
+    {img: 'https://i.imgur.com/WZLyW1b.jpg', matched: false}
+  ];
+  const CARD_BACK = 'https://i.imgur.com/YMwcs7S.png'; 
+  
+  /*----- app's state (variables) -----*/
+  let cards;  // Array of 16 shuffled card objects
+  let firstCard;  // First card clicked (card object) or null
+  let numBad;
+  let ignoreClicks;
+  
+  /*----- cached element references -----*/
+  const msgEl = document.querySelector('h3');
+  
+  /*----- event listeners -----*/
+  document.querySelector('main').addEventListener('click', handleChoice);
+  playAgain.addEventListener('click', init);
+  /*----- functions -----*/
+  init();
+  
+  // Initialize all state, then call render()
+  function init() {
+    cards = getShuffledCards();
+    firstCard = null;
+    numBad = 0;
+    ignoreClicks = false;
+    render();
+  }
+  
+  function render() {
+    cards.forEach(function(card, idx) {
+      const imgEl = document.getElementById(idx);
+      const src = (card.matched || card === firstCard) ? card.img : CARD_BACK;
+      imgEl.src = src;
+    });
+    msgEl.innerHTML = `Bad Count: ${numBad}`;
+  }
+  
+  function getShuffledCards() {
+    let tempCards = [];
+    let cards = [];
+    for (let card of SOURCE_CARDS) {
+      tempCards.push({...card}, {...card});
+    }
+    while (tempCards.length) {
+      let rndIdx = Math.floor(Math.random() * tempCards.length);
+      let card = tempCards.splice(rndIdx, 1)[0];
+      cards.push(card);
+    }
+    return cards;
+  }
+  
+  // Update all impacted state, then call render()
+  function handleChoice(evt) {
+    const cardIdx = parseInt(evt.target.id);
+    if (isNaN(cardIdx) || ignoreClicks) return;
+    const card = cards[cardIdx];
+    if (firstCard) {
+      if (firstCard.img === card.img) {
+        // correct match
+        firstCard.matched = card.matched = true;
+      } else {
+        numBad++;
+      }
+      firstCard = null;
+    } else {
+      firstCard = card;
+    }
+    render();
+  }
